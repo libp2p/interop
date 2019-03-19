11586b924511b7ab849d6033cbb8c4083957eba3
Mikerah <mikerahqc@protonmail.com>
2019-03-18 23:57:29 -0400

    test: tests for switch between 2 go peers

--- test/switch/go2go.js
+++ test/switch/go2go.js
@@ -7,13 +7,19 @@ chai.use(require('chai-bytes'))
 const expect = chai.expect
 
 const CID = require('cids')
-const spawnDaemons = require('../utils/spawnDaemons')
-
+const Switch = require('libp2p-switch')
 const PeerInfo = require('peer-info')
+const PeerBook = require('peer-book')
+const multiplex = require('pull-mplex')
+
+const spawnDaemons = require('../utils/spawnDaemons')
 
 describe('Checking protocols', () => {
   let daemons
   let identify0
+  let switch0
+  let switch1
+
 
   // Start Daemons
   before(async function () {
@@ -22,9 +28,18 @@ describe('Checking protocols', () => {
     daemons = await spawnDaemons(2, 'go')
 
     // connect them
-    identify0 = await daemons[0].client.identify()
+    /*identify0 = await daemons[0].client.identify()
+    identify1 = await daemons[1].client.identify()
+    console.log(daemons[0].client)
 
     await daemons[1].client.connect(identify0.peerId, identify0.addrs)
+ 
+    switch0 = new Switch(new PeerInfo(idenitfy0.peerId), new PeerBook())
+    switch1 = new Switch(new PeerInfo(identify1.peerId), new PeerBook())
+
+    switch0.connection.addStreamMuxer(multiplex)
+    switch1.connection.addStreamMuxer(multiplex)
+*/
 
     // get the peers in the table
     await new Promise(resolve => setTimeout(resolve, 1000))
@@ -40,18 +55,27 @@ describe('Checking protocols', () => {
   it('go peer to go peer', async function () {
     this.timeout(30 * 1000)
 
-    //const cid = new CID('QmVzw6MPsF96TyXBSRs1ptLoVMWRv5FCYJZZGJSVB2Hp39')
+   let protocol = '/test/1.0.0'
+   switch1.handle(protocol, () => { })
 
-    let peerIDs = await daemons[0].client.listPeers()
-    let peers = []
-    for (let peerID of peerIDs) {
-      peers.push(new PeerInfo.create(peerID))
-    }
+   switch0._peerBook.get(switch1._peerInfo).protocols.clear()
 
-    expect(peers.length).to.not.equal(0)
-    for (let peer of peers) {
-      console.log(peers.protocols)
-      expect(peers.protocols).to.exist()
-    }
+   const connFSM = switch0.dialFSM(switch1._peerInfo, protocol, (err) => {
+     expect(err).to.not.exist()
+   })
+
+   connFSM.once('muxed', () => {
+     const peer = switch0._peerBook.get(switch1._peerInfo)
+     const protocols = Array.from(peer.protocols.values())
+     expect(protocols).to.eql([
+       multiplex.multicodec,
+       identify.multicodec,
+       protocol
+     ])
+     connFSM.close()
+   })
+
+   connFSM.once('close', done)
+
   })
 })
