76633f7d7795dff72102a55976bae6459840a4c7
Alex Potsides <alex@achingbrain.net>
2023-08-15 18:10:54 +0100

    deps: switch it-pb-stream for it-protobuf-stream (#121)
    Switches modules

--- src/relay/util.ts
+++ src/relay/util.ts
@@ -1,5 +1,5 @@
-import { pbStream } from 'it-pb-stream'
 import { pipe } from 'it-pipe'
+import { pbStream } from 'it-protobuf-stream'
 import { HopMessage } from './pb/index.js'
 import type { Daemon } from '../index.js'
 import type { PeerId } from '@libp2p/interface-peer-id'
@@ -11,12 +11,12 @@ const RELAY_V2_HOP = '/libp2p/circuit/relay/0.2.0/hop'
 export const reserve = async (d: Daemon, peerID: PeerId, message?: Partial<HopMessage>): Promise<HopMessage> => {
   const stream = await d.client.openStream(peerID, RELAY_V2_HOP)
   const pb = pbStream(stream)
-  pb.writePB({
+  await pb.write({
     type: HopMessage.Type.RESERVE,
     ...(message ?? {})
   }, HopMessage)
 
-  return pb.readPB(HopMessage)
+  return pb.read(HopMessage)
 }
 
 export const echoHandler = {
